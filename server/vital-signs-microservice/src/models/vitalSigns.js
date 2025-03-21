19654b073cb282e0a3dc6e08def4f69f2b7eb200
Lucianna Mendonca <luciannamend@gmail.com>
2025-03-21 16:22:34 -0400

    Add vital signs model

---
+++ server/vital-signs-microservice/src/models/vitalSigns.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const vitalSignsSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, "User is required"],
+    },
+    heartRate: {
+        type: String,
+        required: [true, "Heart rate is required"],
+        min: 30,  // Set reasonable lower limit
+        max: 200, // Set reasonable upper limit
+    },
+    bloodPressure: {
+        type: String,
+        required: [true, "Blood pressure is required"],
+    },
+    temperature: {
+        type: String,
+        required: [true, "Temperature is required"],
+        min: 30,
+        max: 45,
+    }
+}, { timestamps: true });
+
+const VitalSigns = mongoose.model('VitalSigns', vitalSignsSchema);
+
+
+export default VitalSigns;
